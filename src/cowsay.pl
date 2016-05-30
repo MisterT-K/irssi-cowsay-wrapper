e7d65bdacae01409bccc70de1ddb31bead4157b3
Tommi Kärkäs <tommi.karkas@cgi.com>
2016-05-30 13:03:01 +0300

    [Update] First fully working version without injection possibilities

--- src/cowsay.pl
+++ src/cowsay.pl
@@ -1,12 +1,13 @@
-# Depends: LibGetopt Mixed
+# Depends: LibGetopt Long
 
 use strict;
 use vars qw($VERSION %IRSSI);
 
-use Getopt::Mixed;
+# use Getopt::Mixed;
+use Getopt::Long qw(GetOptionsFromString); # 
 use Irssi;
 
-Getopt::Mixed::init( 'f:s');
+# Getopt::Mixed::init( 'f:s');
 $VERSION = '1.00';
 %IRSSI = (
     authors     =>  'Mr. T. Kärkäs',
@@ -17,20 +18,38 @@ $VERSION = '1.00';
                     'through cowsay.',
     license     =>  'Public Domain',
 );
-my $animal = "default";
-@animals = ["apt", "beavis.zen", "bong", "bud-frogs", "bunny", "calvin", "cheese", "cock", "cower", "daemon", "default", "dragon", "dragon-and-cow", "duck", "elephant", "elephant-in-snake", "eyes", "flaming-sheep", "ghostbusters", "gnu", "head-in", "hellokitty", "kiss", "kitty", "koala", "kosh", "luke-koala", "mech-and-cow", "meow", "milk", "moofasa", "moose", "mutilated", "pony", "pony-smaller", "ren", "sheep", "skeleton", "snowman", "sodomized-sheep", "stegosaurus", "stimpy", "suse", "three-eyes", "turkey", "turtle", "tux", "unipony", "unipony-smaller", "vader", "vader-koala", "www"];
 
-# Usage: /COWSAY [<msg>|fortune]
+# Usage: /COWSAY [[-f animal] <msg>|fortune]
 sub cmd_cowsay {
     # data - contains the parameters for /COWSAY
     # server - the active server in window
     # witem - the active window item (eg. channel, query)
     #         or undef if the window is empty
     my ($data, $server, $witem) = @_;
+    my $animal = "default";
+    my $animalCandidate = '';
+    my %animals;
+    my $ret;
+    my $args;
+
+    # Fill in animal options to hash for search purposes.
+    foreach("apt", "beavis.zen", "bong", "bud-frogs", "bunny", "calvin", "cheese", "cock", "cower", "daemon", "default", "dragon", "dragon-and-cow", "duck", "elephant", "elephant-in-snake", "eyes", "flaming-sheep", "ghostbusters", "gnu", "head-in", "hellokitty", "kiss", "kitty", "koala", "kosh", "luke-koala", "mech-and-cow", "meow", "milk", "moofasa", "moose", "mutilated", "pony", "pony-smaller", "ren", "sheep", "skeleton", "snowman", "sodomized-sheep", "stegosaurus", "stimpy", "suse", "three-eyes", "turkey", "turtle", "tux", "unipony", "unipony-smaller", "vader", "vader-koala", "www"){
+        $animals{$_} = 1;
+    }
+    # Check if connected
     if (!$server || !$server->{connected}) {
         Irssi::print("Not connected to server");
         return;
     }
+
+    ($ret, $args) = GetOptionsFromString($data, "f:s" => \$animalCandidate); # flag, optional animal
+    
+    # If animal candidate in parameters is good-to-go, use that
+    if(exists $animals{$animalCandidate}){
+        $animal = $animalCandidate;
+    }
+
+
     if ($witem) {
         # theres query/channel active in window
         my $r_v = -1;
@@ -39,8 +58,10 @@ sub cmd_cowsay {
             @output = qx{fortune|cowsay -f "$animal"};
             $r_v = $?;
         } else {
-            # Someone'll kill me for this, but quotes don't prevent -f injections in the command so... through echo we go
-            @output = qx{echo "$data"|cowsay -f "$animal"};
+            # Parse other text, outside of options...
+            my $inputText = join(' ', @$args);
+
+            @output = qx{echo "$inputText"|cowsay -f "$animal"};
             $r_v = $?;
         }
         # Handle errors with commands cowsay and fortune
@@ -56,17 +77,6 @@ sub cmd_cowsay {
     }
 }
 
-while( my( $option, $value, $pretty ) = Getopt::Mixed::nextOption() ) {
-    OPTION: {
-      $option eq 'f' and $option in @animals do {
-        $animal = $value;
-
-        last OPTION;
-      };
-      # ...
-    }
-}
-Getopt::Mixed::cleanup();
-
+# Bind the command to irssi
 Irssi::command_bind('cowsay', 'cmd_cowsay');
 
