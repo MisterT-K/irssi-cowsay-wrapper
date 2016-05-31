52d89ec95a65726ac0719c7b95f327e234315c2e
Tommi Kärkäs <tommi.karkas@cgi.com>
2016-05-31 08:48:56 +0300

    [Update] Fortune texts now initiated with a separate text.
    Added more comments

--- src/cowsay.pl
+++ src/cowsay.pl
@@ -19,7 +19,17 @@ $VERSION = '1.00';
     license     =>  'Public Domain',
 );
 
-# Usage: /COWSAY [[-f animal] <msg>|fortune]
+#Animal candidate hash list
+
+my %animals;
+
+# Fill in animal options to hash for search purposes.
+
+foreach("apt", "beavis.zen", "bong", "bud-frogs", "bunny", "calvin", "cheese", "cock", "cower", "daemon", "default", "dragon", "dragon-and-cow", "duck", "elephant", "elephant-in-snake", "eyes", "flaming-sheep", "ghostbusters", "gnu", "head-in", "hellokitty", "kiss", "kitty", "koala", "kosh", "luke-koala", "mech-and-cow", "meow", "milk", "moofasa", "moose", "mutilated", "pony", "pony-smaller", "ren", "sheep", "skeleton", "snowman", "sodomized-sheep", "stegosaurus", "stimpy", "suse", "three-eyes", "turkey", "turtle", "tux", "unipony", "unipony-smaller", "vader", "vader-koala", "www"){
+    $animals{$_} = 1;
+}
+
+# Usage: /COWSAY [--fortune  [-f animal] | [-f animal] <msg>] . See cowsay -l for animal list
 sub cmd_cowsay {
     # data - contains the parameters for /COWSAY
     # server - the active server in window
@@ -28,39 +38,35 @@ sub cmd_cowsay {
     my ($data, $server, $witem) = @_;
     my $animal = "default";
     my $animalCandidate = '';
-    my %animals;
+    my $fortune = 0;
     my $ret;
     my $args;
 
-    # Fill in animal options to hash for search purposes.
-    foreach("apt", "beavis.zen", "bong", "bud-frogs", "bunny", "calvin", "cheese", "cock", "cower", "daemon", "default", "dragon", "dragon-and-cow", "duck", "elephant", "elephant-in-snake", "eyes", "flaming-sheep", "ghostbusters", "gnu", "head-in", "hellokitty", "kiss", "kitty", "koala", "kosh", "luke-koala", "mech-and-cow", "meow", "milk", "moofasa", "moose", "mutilated", "pony", "pony-smaller", "ren", "sheep", "skeleton", "snowman", "sodomized-sheep", "stegosaurus", "stimpy", "suse", "three-eyes", "turkey", "turtle", "tux", "unipony", "unipony-smaller", "vader", "vader-koala", "www"){
-        $animals{$_} = 1;
-    }
+    
     # Check if connected
     if (!$server || !$server->{connected}) {
         Irssi::print("Not connected to server");
         return;
     }
 
-    ($ret, $args) = GetOptionsFromString($data, "f:s" => \$animalCandidate); # flag, optional animal
+    ($ret, $args) = GetOptionsFromString($data, "f:s" => \$animalCandidate, "fortune" => \$fortune); # flag, optional animal
     
     # If animal candidate in parameters is good-to-go, use that
     if(exists $animals{$animalCandidate}){
         $animal = $animalCandidate;
     }
-
-
+    
+    # If there is an active window, run cowsay [+fortune] with correct input
     if ($witem) {
         # theres query/channel active in window
         my $r_v = -1;
         my @output;
         
-        # Parse other text, outside of options...
-        my $inputText = join(' ', @$args);
-
-        if($inputText eq "fortune"){
+        if($fortune eq 1){
             @output = qx{fortune|cowsay -f "$animal"};
         } else {
+            # Rest of the input for the command (outside of -f target)
+            my $inputText = join(' ', @$args);
             @output = qx{echo "$inputText"|cowsay -f "$animal"};
         }
         $r_v = $?;
